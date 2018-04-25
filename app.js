ae3fdb3aa203b365151d71cc8e2353489d811ad5
root <root@rev-115.c7d-firewalla-slc.syptec.com>
2018-04-25 11:41:19 -0600

    Open ajax access *

--- app.js
+++ app.js
@@ -66,7 +66,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://eosslc.com, http://eostribe.io');
+    res.setHeader('Access-Control-Allow-Origin', '*');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT');
